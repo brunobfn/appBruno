602be43b916faf261fc6aea728cd116d6a080c98
brunobfn <bruno.norberto@estudante.ifms.edu.br>
2024-03-26 08:53:45 -0400

    aulaExemplo

---
+++ App.js
@@ -0,0 +1,23 @@
+// //npm install -g npm@10.5.0
+
+// // 
+// // estrutura básica de um Aplicativo
+// import React from "react";
+// import { View, Text } from "react-native";
+
+// //sempre tem q ter o return nas funções
+// function App() {
+//   return(
+//    // View é uma tela
+//    <View>
+//       <Text>Olá Mundo!</Text>
+//     </View>
+//   );
+// }
+
+// //depois de criar tenho que colocar o export
+// export default App; //poderia por antes da function
+
+
+
+
